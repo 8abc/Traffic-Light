a2dbc96478f60d8b9416f81fdeb319c4b3ee7369
8abc <anadelaine.catalla@gmail.com>
2019-10-16 23:45:49 -0700

    added onClick, state and finished traffic light

--- traffic/src/components/TrafficLight.jsx
+++ traffic/src/components/TrafficLight.jsx
@@ -2,13 +2,38 @@ import React from "react";
 import "../style.css";
 
 export default class TrafficLight extends React.Component {
+  //this is needed for state
+  constructor() {
+    super();
+    //this is the default value when the page loads
+    this.state = {
+      clickedLight: null
+    };
+  }
   render() {
+    console.log(this.state);
+    const clickedLight = this.state.clickedLight;
+    let redExtraClass = "";
+    if (clickedLight == "red") redExtraClass = "clicked";
+    let yellowExtraClass = "";
+    if (clickedLight == "yellow") yellowExtraClass = "clicked";
+    let greenExtraClass = "";
+    if (clickedLight == "green") greenExtraClass = "clicked";
     return (
       <div>
         <div className="container">
-          <div className="light" id="red"></div>
-          <div className="light" id="yellow"></div>
-          <div className="light" id="green"></div>
+          <div
+            className={"red light " + redExtraClass}
+            onClick={() => this.setState({ clickedLight: "red" })}
+          ></div>
+          <div
+            className={"yellow light " + yellowExtraClass}
+            onClick={() => this.setState({ clickedLight: "yellow" })}
+          ></div>
+          <div
+            className={"green light " + greenExtraClass}
+            onClick={() => this.setState({ clickedLight: "green" })}
+          ></div>
         </div>
       </div>
     );
